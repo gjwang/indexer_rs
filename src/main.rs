1ed2692d6922f198f2ebb7fa24404740801d32d9
0xMaster <guijiewan@gmail.com>
2023-10-05 04:45:33 +0800

    use Active-web to write Hello, worldbalance: 0.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("{\"status\":0, \"msg\":\"ok\"}")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/health", web::get().to(health))
+    })
+        .bind(("127.0.0.1", 8090))?
+        .run()
+        .await
+}
+
