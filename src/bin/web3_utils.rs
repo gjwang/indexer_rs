26acf3fe7cc0b57ae2689c51b604e2b5ca4c5ca0
0xMaster <guijiewan@gmail.com>
2023-10-06 01:00:17 +0800

    use scylldab for best perf

--- src/bin/web3_utils.rs
+++ src/bin/web3_utils.rs
@@ -17,5 +17,17 @@ async fn main() -> web3::Result<()> {
     let block_number = web3.eth().block_number().await?;
     println!("Latest block number: {:?}", block_number);
 
+    println!("Calling accounts.");
+    let mut accounts = web3.eth().accounts().await?;
+    println!("Accounts: {:?}", accounts);
+    accounts.push("00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap());
+
+    println!("Calling balance.");
+    for account in accounts {
+        let balance = web3.eth().balance(account, None).await?;
+        println!("Balance of {:?}: {}", account, balance);
+    }
+
+
     Ok(())
 }
