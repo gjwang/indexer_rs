942391aaa4d1695f68401eefe4c015e8acd3e391
0xMaster <guijiewan@gmail.com>
2023-10-06 01:08:26 +0800

    use env to get SCYLLA_BUNDLE_CONFIG_YAML_FILE

--- src/bin/scylladb_test.rs
+++ src/bin/scylladb_test.rs
@@ -2,12 +2,17 @@ use std::path::Path;
 
 use anyhow::Result;
 use scylla::CloudSessionBuilder;
+extern crate dotenv;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     println!("Connecting to SNI proxy as described in cloud config yaml ...");
+    dotenv::dotenv().expect("Failed to read .env file");
 
-    let session = CloudSessionBuilder::new(Path::new("examples/config_data.yaml"))
+    let bundle_config_yaml_file: &String = &std::env::var("SCYLLA_BUNDLE_CONFIG_YAML_FILE")
+        .expect("scylla_bundle_config_file not found");
+
+    let session = CloudSessionBuilder::new(Path::new(bundle_config_yaml_file))
         .unwrap()
         .build()
         .await
