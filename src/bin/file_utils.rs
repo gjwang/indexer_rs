e909444891e3bc7244852331916645ba514caaf6
0xMaster <guijiewan@gmail.com>
2023-10-07 19:10:39 +0800

    so there a lot of chances the test will failed if not delay a little time

--- src/bin/file_utils.rs
+++ src/bin/file_utils.rs
@@ -3,6 +3,8 @@ use std::fs;
 use std::fs::{File, OpenOptions};
 use std::io::{Error, Write};
 
+use std::thread::sleep;
+use std::time::Duration;
 pub fn create_file_if_not_exists(path: &str) -> std::io::Result<File> {
     OpenOptions::new().write(true).create(true).open(path)
 }
@@ -11,12 +13,18 @@ pub fn create_file_if_not_exists(path: &str) -> std::io::Result<File> {
 mod tests {
     use super::*;
     use std::path::Path;
+    use std::time;
 
     const TEST_FILE: &str = "test_file.txt";
 
     fn cleanup() {
+        //Maybe rust is running too fast,
+        // so there a lot of chances the test will failed if not delay a little time
+        sleep(Duration::from_millis(10));
         if Path::new(TEST_FILE).exists() {
             fs::remove_file(TEST_FILE).unwrap();
+            sleep(Duration::from_millis(10));
+
         }
     }
 
@@ -39,14 +47,13 @@ mod tests {
         {
             let mut file = fs::File::create(TEST_FILE).unwrap();
             file.write_all(b"Some data").unwrap();
+            // Ensure all data is flushed to the disk
+            file.flush().unwrap();
         }
 
         let before = fs::read(TEST_FILE).unwrap();
         create_file_if_not_exists(TEST_FILE).unwrap();
         let after = fs::read(TEST_FILE).unwrap();
-
-        println!("before {:?}", before);
-        println!("after  {:?}", after);
 
         assert_eq!(before, after); // Ensure the file content hasn't changed
 
@@ -58,4 +65,8 @@ mod tests {
     fn fails_with_invalid_file_name() {
         create_file_if_not_exists("").unwrap();
     }
+}
+
+fn main() {
+
 }
